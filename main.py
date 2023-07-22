bf7bde436d77142d29d878e8bcf63de6aa5c3db3
321999 <73833132+321999@users.noreply.github.com>
2023-07-22 14:14:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def add(a,b):
+    print("printing the value")
+    return a + b
+
+print(add(2,6))
+
+# def add(a, b):
+#     print("printing the value")
+#     return a + b
+
+# print(add(2, 6))
+
+
+
+
