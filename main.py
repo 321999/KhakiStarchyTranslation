a28491d2607baa4fa710f3b5b9dfe94e662020e7
kishorejha <5157107-kishorejha@users.noreply.replit.com>
2023-07-22 18:20:53 +0000

    recursion concept clear

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 
-# def add(a,b):
-#     print("printing the value")
-#     return a + b
-
-# print(add(2,6))
-
-# # def add(a, b):
+# # def add(a,b):
 # #     print("printing the value")
 # #     return a + b
 
-# # print(add(2, 6))
+# # print(add(2,6))
+
+# # # def add(a, b):
+# # #     print("printing the value")
+# # #     return a + b
+
+# # # print(add(2, 6))
 
 
-# recursion in git 
-print("dsa in python ")
+# # recursion in git 
+# print("dsa in python ")
+# # recursion means when function call itself again and again or at every cucle of execution then it is the case of recursion 
+# c=0
+# j=0
+# def add(a,b):
+#   return a+b
+# for i in range(1,2,10):
+#   # j=add(1,2)
+#   j+=add(1,2)
+# print(j)
 
+# # additoin of number without recursion 
+# # additoin of numbert from 1 to 10
+# def sum(a,b):
+#   return a+b
+# sum(2,3)
 
+# s=0
+# for i in range(1,10,2):
+#   s=i+s
+# print(s)  
 
+# def joro(a,b,c):
+#   while k<c:
+#     a+b
+    
+#   if c!=10:
+#     j=a+b
+    
+# joro(1,2,10)
+  
+#   # c+=1
+#   # if c==10:
+#   #   break
+# add(3,4)           
